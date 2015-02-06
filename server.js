22b47142c83940a4b7b7d0f135f389a2d06f0c47
Tomasz Jankowski <tomasz.jankowski@avaus.fi>
2015-02-06 12:41:35 +0100

    Configure server to send messages to all receivers

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
+
 var WebSocketServer = require('websocket').server;
 var http = require('http');
+var _ = require('lodash');
+var Firebase = require("firebase");
 
 var server = http.createServer(function(request, response) {
     console.log((new Date()) + ' Received request for ' + request.url);
@@ -22,31 +25,62 @@ wsServer = new WebSocketServer({
 });
 
 function originIsAllowed(origin) {
-  // put logic here to detect whether the specified origin is allowed.
-  return true;
+    // put logic here to detect whether the specified origin is allowed.
+    return true;
 }
+
+function sendMsg(source, message) {
+    console.log('Sending message to ' + connections.length + ' receivers');
+    connections.forEach(function(output) {
+        var msg = JSON.stringify({
+            type: 'message',
+            source: source,
+            data: message
+        });
+        output.sendUTF(msg);
+    });
+}
+
+var connections = [];
+var ids = 0;
 
 wsServer.on('request', function(request) {
     if (!originIsAllowed(request.origin)) {
-      // Make sure we only accept requests from an allowed origin
-      request.reject();
-      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
-      return;
+        // Make sure we only accept requests from an allowed origin
+        request.reject();
+        console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+        return;
     }
 
+    // Accept connection
     var connection = request.accept('echo-protocol', request.origin);
-    console.log((new Date()) + ' Connection accepted.');
+    connection.connectionId = ++ids;
+    console.log((new Date()) + ' Connection ' + connection.connectionId + ' accepted.');
+    
+    connections.push(connection);
+
+    // Send identifier to client
+    connection.sendUTF(JSON.stringify({
+        type: 'identifier',
+        data: connection.connectionId
+    }));
+
+    // Send hello message
+    sendMsg(connection.connectionId, 'Has joined chat');
+
     connection.on('message', function(message) {
         if (message.type === 'utf8') {
             console.log('Received Message: ' + message.utf8Data);
-            connection.sendUTF(message.utf8Data);
-        }
-        else if (message.type === 'binary') {
+            sendMsg(connection.connectionId, message.utf8Data);
+        } else if (message.type === 'binary') {
             console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
             connection.sendBytes(message.binaryData);
         }
     });
+
     connection.on('close', function(reasonCode, description) {
         console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+        sendMsg(connection.connectionId, 'Has left chat');
+        _.remove(connections, connection);
     });
 });
